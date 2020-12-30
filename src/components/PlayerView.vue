336aceae1a53065ee7cea6ada23fb2868048e803
Bridger Herman <herma582@umn.edu>
2020-12-30 11:52:42 -0600

    Add sqlite and express server

--- src/components/PlayerView.vue
+++ src/components/PlayerView.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="frame">
     {{ this.$store.state.count }}
+
   </div> 
 </template>
 
 <script>
-export default {
+import api from '@/api'
 
+export default {
+  async created () {
+    console.log(api.createPost({title: 'test', body: 'test'}));
+    api.getPosts().then((post) => console.log(post));
+  },
 }
 </script>
 
